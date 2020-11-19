a1e14555c954ae717f787810ff57654390dc5944
Abhijit Singh <abhijit@noonacademy.com>
2020-11-19 11:52:31 +0530

    Initial project setup

---
+++ BouncyBallsLoader/BouncyBallsLoader.h
@@ -0,0 +1,18 @@
+//
+//  BouncyBallsLoader.h
+//  BouncyBallsLoader
+//
+//  Created by Abhijit Singh on 19/11/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BouncyBallsLoader.
+FOUNDATION_EXPORT double BouncyBallsLoaderVersionNumber;
+
+//! Project version string for BouncyBallsLoader.
+FOUNDATION_EXPORT const unsigned char BouncyBallsLoaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BouncyBallsLoader/PublicHeader.h>
+
+
---
+++ BouncyBallsLoaderExamples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BouncyBallsLoaderExamples
+//
+//  Created by Abhijit Singh on 19/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
